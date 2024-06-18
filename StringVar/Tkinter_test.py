db65fd12eade77e59ce090fb9ea7292bae93e054
Alkindy-tech <naamanalkindy92@gmail.com>
2024-06-18 17:30:32 +0300

    my first python class

---
+++ StringVar/Tkinter_test.py
@@ -0,0 +1,6 @@
+from tkinter import messagebox
+myTitle = "Alkindy Barrister"
+person = "Mr. Alkindy "
+personsaid = "Enjoy the coffee"
+quote = person + personsaid
+messagebox.showinfo(myTitle, quote)
---
+++ StringVar/logic.py
@@ -0,0 +1,9 @@
+#from tkinter import messagebox
+#name = "Alkindy"
+age = input("How old are you?\n")
+if int(age) == 21:
+    print("You are 21")
+    #messagebox.showinfo(name, "You are 21")
+elif int(age) == 22:
+    print("You are 22")
+    #messagebox.showinfo(name, "you are 22")
