3a78230826c34ef225c9ed7735ca7f73fc18f15a
Alkindy-tech <naamanalkindy92@gmail.com>
2024-06-20 17:57:00 +0300

    my first python class

---
+++ StringVar/for_loops.py
@@ -0,0 +1,12 @@
+"""
+multiplyBy = 7
+for current_range_number in range(1, 21):
+    answer = current_range_number * multiplyBy
+    #print(answer)
+    print(str(current_range_number) + " * " + str(multiplyBy) + " = " + str(answer))
+"""
+students = ["Fredrick Mukoba", "Matt Murrdock", "Alkindy"]
+for x in students:
+    print(x)
+    if x == "Matt Murrdock":
+        break
