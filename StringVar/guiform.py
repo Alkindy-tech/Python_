74c6fb1c120df7eec01e7338014a7ae537749f1a
Alkindy-tech <naamanalkindy92@gmail.com>
2024-06-25 09:43:55 +0300

    my first python class

---
+++ StringVar/guiform.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+form = tk.Tk()
+form.title("My first Form")
+alkindyLabel = tk.Label(form, text="This is a Label",
+                        width=25, height=5, bg="gold").pack()
+form.mainloop()  #this line of code comes at the end.
+
+
